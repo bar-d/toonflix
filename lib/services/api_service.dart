6d92c382a4cfe69a75f49a18f2326e9ab79914de
bard <schy2503@naver.com>
2023-06-13 17:56:17 +0900

    feat: APIService 구현

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,16 +1,26 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
 
 class APIService {
   final String bastURL = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse("$bastURL/$today");
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(response.body);
+
+      for (var webtoon in webtoons) {
+        final toon = WebtoonModel.fromJson(webtoon);
+        print(toon.title);
+        webtoonInstances.add(toon);
+      }
+      return webtoonInstances;
     }
 
     throw Error();
